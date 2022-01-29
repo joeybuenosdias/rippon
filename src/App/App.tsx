6ae9dfe4f19ca846c450e0cd8093e4a433c0c815
Joey Schrader <dev@joeyschrader.com>
2022-01-29 08:24:12 -0800

    Add Header component and configure absolute paths

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import type { ReactElement } from "react";
 import TodoList from "../components/TodoList";
+import Header from "../components/Header";
 
 export default function App(): ReactElement {
   return (
     <div>
+      <Header />
       <TodoList />
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import type { ReactElement } from "react";
+
+export default function Header(): ReactElement {
+  return <nav>Header</nav>;
+}
